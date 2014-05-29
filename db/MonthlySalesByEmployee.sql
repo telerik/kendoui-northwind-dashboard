395d2b89b0ddf7af0fe26c5c53e36501fbf2df00
Alexander Popov <alexander.popov@telerik.com>
2014-05-29 15:04:43 +0300

    Removed temporary files

--- db/MonthlySalesByEmployee.sql
+++ db/MonthlySalesByEmployee.sql
@@ -9,7 +9,7 @@ BEGIN
 	SET NOCOUNT ON;
 
 		SELECT  Orders.EmployeeID, 
-				SUM((Quantity * UnitPrice) - (Quantity * UnitPrice * Discount))  AS Amount,  
+				SUM((Quantity * UnitPrice) - (Quantity * UnitPrice * Discount))  AS EmployeeSales,  
 				CAST(CONVERT(VARCHAR, DATEPART(YEAR, Orders.OrderDate)) + '-' + CONVERT(VARCHAR, DATEPART(MONTH, Orders.OrderDate)) + '-1'  AS DATETIME) AS Date
 		FROM [Order Details]
 			INNER JOIN Orders ON Orders.OrderID = [Order Details].OrderID
--- db/SQLQuery4.sql
+++
@@ -1,7 +0,0 @@
-SELECT Orders.OrderDate, SUM(Details.Amount) AS Amount FROM Orders 
-INNER JOIN (
-	SELECT Orders.OrderID, sum((Quantity * UnitPrice) - (Quantity * UnitPrice * Discount))  AS Amount FROM [Order Details] AS Details
-	INNER JOIN Orders ON Orders.OrderID = Details.OrderID
-	GROUP BY Orders.OrderID
-) AS Details  ON Orders.OrderID = Details.OrderID 
-GROUP BY Orders.OrderDate
