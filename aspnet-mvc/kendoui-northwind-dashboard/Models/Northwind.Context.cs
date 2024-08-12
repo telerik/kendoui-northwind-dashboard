3820847cf0550ca3c9edd69c4e9b92582d10b32e
Anton_Mironov <anton.mironov@progress.com>
2024-08-12 13:47:57 +0300

    add changes after review of the PR for the sqlite migration

--- aspnet-mvc/kendoui-northwind-dashboard/Models/Northwind.Context.cs
+++ aspnet-mvc/kendoui-northwind-dashboard/Models/Northwind.Context.cs
@@ -36,20 +36,6 @@
         public DbSet<Shipper> Shippers { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<Territory> Territories { get; set; }
-        //public DbSet<Alphabetical_list_of_product> Alphabetical_list_of_products { get; set; }
-        //public DbSet<Category_Sales_for_1997> Category_Sales_for_1997 { get; set; }
-        //public DbSet<Current_Product_List> Current_Product_Lists { get; set; }
-        //public DbSet<Invoice> Invoices { get; set; }
-        //public DbSet<Order_Details_Extended> Order_Details_Extendeds { get; set; }
-        //public DbSet<Order_Subtotal> Order_Subtotals { get; set; }
-        //public DbSet<Orders_Qry> Orders_Qries { get; set; }
-        //public DbSet<Product_Sales_for_1997> Product_Sales_for_1997 { get; set; }
-        //public DbSet<Products_Above_Average_Price> Products_Above_Average_Prices { get; set; }
-        //public DbSet<Products_by_Category> Products_by_Categories { get; set; }
-        //public DbSet<Sales_by_Category> Sales_by_Categories { get; set; }
-        //public DbSet<Sales_Totals_by_Amount> Sales_Totals_by_Amounts { get; set; }
-        //public DbSet<Summary_of_Sales_by_Quarter> Summary_of_Sales_by_Quarters { get; set; }
-        //public DbSet<Summary_of_Sales_by_Year> Summary_of_Sales_by_Years { get; set; }
         public DbSet<CustomerDemographic> CustomerDemographics { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
